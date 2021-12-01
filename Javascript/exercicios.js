b6b5d01581bc7c6e5729f597f5d003d0b0e85ce9
Danilo <dansdeiro@gmail.com>
2021-12-01 11:49:18 -0300

    Exercicios Javascript resolvidos 1 ao 9

---
+++ Javascript/exercicios.js
@@ -0,0 +1,92 @@
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; // Exercicio 1
+
+// for (let index of numbers) {
+//     console.log(index)
+// }
+
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; // Exercicio 2
+// var soma = 0;
+// for (let index = 0; index<numbers.length; index +=1){
+//     soma += numbers[index]
+// }
+// console.log(soma)
+
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; // Exercicio 3
+// let mediaArit = 0;
+
+// for (let index = 0; index < numbers.length; index +=1){
+//     mediaArit += numbers[index] / (numbers.length);
+// }
+// console.log(mediaArit);
+
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; //Exercicio 4
+// let mediaArit = 0;
+
+// for (let index = 0; index < numbers.length; index +=1){
+//     mediaArit += numbers[index] / (numbers.length);
+// }
+// if (mediaArit > 20) {
+// console.log(mediaArit, " valor maior do que 20");
+// }
+// else {
+// console.log (mediaArit, " valor menor ou igual a 20");
+// }
+
+
+// let numbers = [5, 9, 3, 19, 18, 8, 10, 2, 35, 27]; // Exercicio 5
+// let maior = 0;
+
+// for (let index = 0; index < numbers.length; index +=1){
+//     if (numbers[index] > maior){
+//             maior = numbers[index];
+//         }
+//     }
+//     console.log("o maior valor é: ", maior);
+
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; // Exercicio 6
+// somaImpares = 0;
+// for (let index = 0; index < numbers.length; index +=1){
+//     if (numbers[index] %2 !== 0){
+//         somaImpares += 1;
+//     }
+// }
+// console.log("existem ", somaImpares, "numeros impares");
+
+
+// let numbers = [10, 9, 3, 1, 70, 8, 100, 2, 35, 27]; // Exercicio 7
+// let menor = numbers[0];
+
+// for (let index = 1; index < numbers.length; index +=1){
+//     if (numbers[index] < menor){
+//             menor = numbers[index];
+//         }
+//     }
+   
+//     console.log("o menor valor é: ", menor);
+
+// let numbers = [] //Exercicio 8
+// let valores = 1
+// for (let index = 0; index < 25; index +=1){
+// numbers[index] = valores
+// valores += 1
+// }
+// console.log(numbers)
+
+// let numbers = [] //Exercicio 9
+// let valores = 1
+// for (let index = 0; index < 25; index +=1){
+// numbers[index] = valores
+// valores += 1
+// }
+// console.log(numbers);
+
+// let divisao = []
+
+// for (let index2 = 0; index2 <numbers.length ; index2 +=1) {
+//     divisao[index2] = (numbers[index2]) / 2 
+// }
+// console.log(divisao)
